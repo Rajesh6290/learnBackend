a3cba068dc8477a42aae59accce4f1a1375d45ca
Rajesh6290 <rajeshkumarbehera039@gmail.com>
2024-01-04 09:20:01 +0530

    chnags

--- src/index.js
+++ src/index.js
@@ -1,4 +1,11 @@
-import dotenv from "dotenv";
-dotenv.config({ path: "./env" });
+import { config } from "dotenv";
+config();
+import { app } from "./app.js";
 import connectDB from "./db/index.js";
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log(`Port running at ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => console.log(err));
---
+++ src/models/user.model.js
@@ -0,0 +1,58 @@
+import mongoose, { Schema } from "mongoose";
+import { Jwt } from "jsonwebtoken";
+import bcrypt from "bcrypt";
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      index: true,
+      required: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      required: true,
+    },
+    fullname: {
+      type: String,
+      lowercase: true,
+      trim: true,
+      required: true,
+    },
+    avatar: {
+      type: String,
+      trim: true,
+    },
+    coverimage: {
+      type: String,
+    },
+    watchhistory: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Video",
+      },
+    ],
+    password: {
+      type: String,
+      required: [true, "required"],
+    },
+    refreshtoken: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = bcrypt.hash(this.password, 10);
+  next();
+});
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+export const User = mongoose.model("User", userSchema);
